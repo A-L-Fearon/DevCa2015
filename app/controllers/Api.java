50778c60b0d5455be5e9652a0d41a7f48fa838dd
Antonio Fearon <antonio.l.fearon@gmail.com>
2015-03-05 17:18:17 -0500

    .

---
+++ app/controllers/Api.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import play.mvc.Controller;
+
+/**
+ * Created by fearon on 3/5/15.
+ */
+public class Api extends Controller {
+
+
+
+}
--- app/models/Products.java
+++ app/models/Products.java
@@ -3,6 +3,7 @@ package models;
 import play.db.ebean.Model;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 
 /**
  * Created by fearon on 3/5/15.
@@ -12,5 +13,7 @@ public class Products extends Model {
     public String name;
     public Long productID;
     public Integer price;
-
+    @ManyToOne
+    public Stores store;
+    public static Finder<String,Products> find = new Finder<>(String.class, Products.class);
 }
