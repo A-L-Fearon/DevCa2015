70df6d7df5c010df605de48bb39af1e15cb07b58
Antonio Fearon <antonio.l.fearon@gmail.com>
2015-03-06 06:12:22 -0500

    .

--- app/controllers/api/GroceryController.java
+++ app/controllers/api/GroceryController.java
@@ -1,23 +1,35 @@
 package controllers.api;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import models.Product;
 import models.Store;
+import play.data.DynamicForm;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.*;
+import play.data.Form;
 
 import views.html.*;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Shane on 3/5/2015.
  */
 public class GroceryController extends Controller {
 
-    @BodyParser.Of(BodyParser.Json.class)
-    public static Result myList(String groceries) {
+    //@BodyParser.Of(BodyParser.Json.class)
+    public static Result myList() {
+        Map<String, String[]> body = request().body().asFormUrlEncoded();
+        //JsonNode json = request().body().asJson();
+
+        System.out.print(request().body().asRaw());
+        return ok("Got body: ");
+
+        /*
         int i, x, lowid = 0, prods, len;
         float  lowest = 0;
         float[] storenum = new float[50];
@@ -52,6 +64,46 @@ public class GroceryController extends Controller {
         result.put("storename", storenum[lowid]);
         result.put("latitude", products.get(lowid).store.latitude);
         result.put("longitude", products.get(lowid).store.longitude);
-        return ok();
+        result.put("what",groceries);
+        result.put("test",id);
+        return ok(result);
+    */}
+
+    public static String FormData2String(Map<String, String[]> data){
+        String retStr = "", key;
+
+        if(data.isEmpty()){
+            return retStr;
+        }
+
+        Iterator<String> keys = data.keySet().iterator();
+        while(keys.hasNext()){
+            key = keys.next();
+            retStr = retStr + "\n" + key + " :=> " + Array2CSV(data.get(key));
+        }
+
+        return retStr;
     }
+
+    public static String Array2CSV(String[] array){
+        String retStr = "";
+        if(array == null){
+            return retStr;
+        }
+        if(array.length == 1){
+            return array[0];
+        }
+
+        for(int c= 0; c < array.length; c++){
+            // last value in array
+            if((array.length -1) == c){
+                retStr = retStr + array[c] + "";
+            }else{
+                retStr = retStr + array[c] + ", ";
+            }
+        }
+
+        return retStr;
+    }
+
 }
