682d3b81734823104a0807edaea710abebab11e5
Antonio Fearon <antonio.l.fearon@gmail.com>
2015-03-06 01:27:54 -0500

    .

--- app/controllers/api/GroceryController.java
+++ app/controllers/api/GroceryController.java
@@ -1,9 +1,12 @@
 package controllers.api;
 
+import models.Store;
 import play.mvc.Controller;
 import play.mvc.*;
 
 import views.html.*;
+
+import java.util.List;
 
 /**
  * Created by Shane on 3/5/2015.
@@ -11,7 +14,14 @@ import views.html.*;
 public class GroceryController extends Controller {
 
     public static Result myList(String groceries) {
+        int i, low;
         String[] groceryArray = groceries.split(",");
+
+        List<Store> stores = Store.find.findList();
+
+        for(i=0; i< stores.size(); i++){
+            i++;//stores.get(i).product.get();
+        }
 
         // TODO
         return ok();
--- app/models/Store.java
+++ app/models/Store.java
@@ -5,6 +5,7 @@ import play.db.ebean.Model;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import java.util.List;
 
 /**
  * Created by fearon on 3/5/15.
@@ -23,13 +24,12 @@ public class Store extends Model {
     public String type;
     public String town;
     public String telenum;
+    //@OneToMany
+    //public Product product;
+    @OneToMany(mappedBy = "store")
+    public List<Product> product;
 
-    @OneToMany
-    public Product product;
-    @OneToMany
-//    public List<Product> product = new ArrayList<Product>();
-
-    public static Finder<String, Store> find = new Finder<>(String.class, Store.class);
+    public static Finder<Long, Store> find = new Finder<>(Long.class, Store.class);
 
     public static Store create (Store store, String name, String parish, Double latitude, Double longitude) {
         store.name = name;
